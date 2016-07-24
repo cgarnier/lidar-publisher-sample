10308054dc08b43cd2702f1546e23b9ef77392e7
Clément Garnier <cle.garnier@gmail.com>
2016-07-24 11:06:13 +0200

    add a todo

--- index.js
+++ index.js
@@ -35,6 +35,7 @@ amqp.connect(Config.amqp, (err, conn) => {
       data = new Uint8Array([ 0xFA ].concat(data.slice(0, 21)))
       try {
         let lp = new LidarPacket(data)
+        // TODO: publish bytes is more efficient, send bytes and decode on client side
         ch.publish(ex, '', new Buffer(JSON.stringify(lp)))
       } catch (e) {
         console.error('Error while parsing packet: ' + e.message)
